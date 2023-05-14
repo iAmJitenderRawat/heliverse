e104d46fec4cee9476f2092a7c8d0c2090a9b143
iAmJitenderRawat <jitenderrawat39@gmail.com>
2023-05-15 02:18:59 +0530

    added env

--- client/src/features/team/teamSlice.js
+++ client/src/features/team/teamSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const customTeamApi = createApi({
   reducerPath: "customTeamApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3003",
+    baseUrl: import.meta.env.VITE_URL,
   }),
   tagTypes: ["Team"],
   endpoints: (builder) => ({
